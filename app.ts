aef46cfbdbf43cf6a46852f359929f280e6a90ff
Mohamed Ayman <mohdayman22rr@gmail.com>
2023-05-11 14:06:58 +0300

    ADD: Global variables

---
+++ app.ts
@@ -0,0 +1,4 @@
+const userNameEl = document.getElementById("username") as HTMLInputElement;
+const emailEl = document.getElementById("email") as HTMLInputElement;
+const passwordEl = document.getElementById("password") as HTMLInputElement;
+const confirmPasswordEl = document.getElementById("confirm-password") as HTMLInputElement;

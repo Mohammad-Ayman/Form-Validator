8fbc7ac878bf7965328148cf63b1d30b93b3707c
Mohamed Ayman <mohdayman22rr@gmail.com>
2023-05-13 01:33:34 +0300

    ADD: js file

---
+++ app.js
@@ -0,0 +1,79 @@
+// Global variables
+var userNameEl = document.getElementById("username");
+var emailEl = document.getElementById("email");
+var passwordEl = document.getElementById("password");
+var confirmPasswordEl = document.getElementById("confirm-password");
+var submit = document.querySelector("button");
+var password;
+// Helper Functions
+var conditions = function (condition, parent, smallEL, message) {
+    if (smallEL)
+        if (condition) {
+            parent === null || parent === void 0 ? void 0 : parent.classList.add("error");
+            smallEL.innerText = message !== null && message !== void 0 ? message : "";
+        }
+        else {
+            parent === null || parent === void 0 ? void 0 : parent.classList.remove("error");
+            parent === null || parent === void 0 ? void 0 : parent.classList.add("success");
+        }
+};
+var confirmConditions = function () { };
+// Global Functions
+var username = function () {
+    var username = userNameEl.value.trim();
+    var parent = userNameEl.parentElement;
+    var small = userNameEl.nextElementSibling;
+    var message = "Username must be at least 3 characters";
+    console.log(small);
+    console.log("username length: ".concat(username.length));
+    var condition = username.length < 3;
+    conditions(condition, parent, small, message);
+};
+var email = function () {
+    var email = emailEl.value.trim();
+    var parent = emailEl.parentElement;
+    var small = emailEl.nextElementSibling;
+    var message = "Email is not valid";
+    console.log("email length: ".concat(email.length));
+    // const condition = !email.includes("@") || !email.endsWith(".com");
+    var condition = email.length < 3;
+    conditions(condition, parent, small, message);
+};
+var passwordFun = function () {
+    password = passwordEl.value.trim();
+    var small = passwordEl.nextElementSibling;
+    var message = "Password must be at least 6 characters";
+    var parent = passwordEl.parentElement;
+    var condition = password.length < 6;
+    console.log("password is: ".concat(password));
+    console.log("password length: ".concat(password.length));
+    conditions(condition, parent, small, message);
+};
+var confirmPassword = function () {
+    var confirmPassword = confirmPasswordEl.value.trim();
+    console.log("confirm Password is: ".concat(confirmPassword));
+    console.log("confirm Password length: ".concat(confirmPassword.length));
+    var confirmParent = confirmPasswordEl.parentElement;
+    var confirmSmall = confirmPasswordEl.nextElementSibling;
+    var confirmMessage;
+    if (confirmPassword === "")
+        confirmMessage = "This field is required";
+    else if (confirmPassword.length > 6)
+        confirmMessage = "Password must be at least 6 characters";
+    else if (password != "" && confirmPassword != password)
+        confirmMessage = "Passwords don't match";
+    else
+        confirmMessage = "Password not valid";
+    var confirmCondition = confirmPassword.length < 6;
+    conditions(confirmCondition, confirmParent, confirmSmall, confirmMessage);
+};
+//
+var submitHandler = function (e) {
+    e.preventDefault();
+    console.log(e);
+    username();
+    email();
+    passwordFun();
+    confirmPassword();
+};
+submit === null || submit === void 0 ? void 0 : submit.addEventListener("click", submitHandler);
--- app.ts
+++ app.ts
@@ -12,23 +12,26 @@ let password: string;
 const conditions = (
   condition: boolean,
   parent: HTMLElement,
-  smallEL,
+  smallEL: HTMLElement | null | undefined,
   message?: string
 ) => {
-  if (condition) {
-    parent?.classList.add("error");
-    smallEL.innerText = message;
-  } else {
-    parent?.classList.remove("error");
-    parent?.classList.add("success");
-  }
+  if (smallEL)
+    if (condition) {
+      parent?.classList.add("error");
+      smallEL.innerText = message ?? "";
+    } else {
+      parent?.classList.remove("error");
+      parent?.classList.add("success");
+    }
 };
+
+const confirmConditions = () => {};
 
 // Global Functions
 const username = () => {
-  const username: string = userNameEl.value;
+  const username: string = userNameEl.value.trim();
   const parent = userNameEl.parentElement as HTMLElement;
-  const small = userNameEl.nextElementSibling;
+  const small = userNameEl.nextElementSibling as HTMLElement;
   const message = "Username must be at least 3 characters";
   console.log(small);
   console.log(`username length: ${username.length}`);
@@ -37,19 +40,19 @@ const username = () => {
 };
 
 const email = () => {
-  const email: string = emailEl.value;
+  const email: string = emailEl.value.trim();
   const parent = emailEl.parentElement as HTMLElement;
-  const small = emailEl.nextElementSibling;
+  const small = emailEl.nextElementSibling as HTMLElement;
   const message = "Email is not valid";
   console.log(`email length: ${email.length}`);
-  // if (!email.includes("@") || !email.endsWith(".com")) {
+  // const condition = !email.includes("@") || !email.endsWith(".com");
   const condition = email.length < 3;
   conditions(condition, parent, small, message);
 };
 
 const passwordFun = () => {
-  password = passwordEl.value;
-  const small = passwordEl.nextElementSibling;
+  password = passwordEl.value.trim();
+  const small = passwordEl.nextElementSibling as HTMLElement;
   const message = "Password must be at least 6 characters";
   const parent = passwordEl.parentElement as HTMLElement;
   const condition = password.length < 6;
@@ -58,11 +61,11 @@ const passwordFun = () => {
   conditions(condition, parent, small, message);
 };
 const confirmPassword = () => {
-  const confirmPassword: string = confirmPasswordEl.value;
+  const confirmPassword: string = confirmPasswordEl.value.trim();
   console.log(`confirm Password is: ${confirmPassword}`);
   console.log(`confirm Password length: ${confirmPassword.length}`);
   const confirmParent = confirmPasswordEl.parentElement as HTMLElement;
-  const confirmSmall = confirmPasswordEl.nextElementSibling;
+  const confirmSmall = confirmPasswordEl.nextElementSibling as HTMLElement;
   let confirmMessage: string;
   if (confirmPassword === "") confirmMessage = "This field is required";
   else if (confirmPassword.length > 6)
